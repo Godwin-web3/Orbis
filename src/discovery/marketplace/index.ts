59431ddd418270dd7f75ece3ce7b5b80fdc8b9a9
GodwinXbt <mbagodwin419@gmail.com>
2026-08-25 09:30:48 +0100

    Remove empty MarketplaceSource stub

--- src/discovery/marketplace/index.ts
+++
@@ -1,7 +0,0 @@
-import type { DiscoverySource } from "../../domain/ports";
-import type { MintCandidate } from "../../domain/types";
-
-export class MarketplaceSource implements DiscoverySource {
-  readonly name = "marketplace";
-  async discover(): Promise<MintCandidate[]> { return []; }
-}
